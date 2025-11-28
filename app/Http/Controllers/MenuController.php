ad21f0d8d154a976c556fc8084313df4af7b25c8
Fransciesco.s <fransciesco.steinlie@gmail.com>
2025-11-28 09:08:01 +0700

    Mengedit models dan controller menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -15,7 +15,7 @@ class MenuController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
-            'ID_PRODUK' => 'required|string|max:10',
+            'ID_PRODUK' => 'required|string|max:10|unique:menu,ID_PRODUK',
             'NAMA_PRODUK' => 'required|string|max:50',
             'HARGA_DASAR' => 'required|integer',
             'HARGA_JUAL' => 'required|integer',
@@ -26,13 +26,18 @@ class MenuController extends Controller
 
     public function show($id)
     {
-        return Menu::findOrFail($id);
+        return Menu::find($id);
     }
 
     public function update(Request $request, $id)
     {
-        $menu = Menu::findOrFail($id);
-        $menu->update($request->all());
+        $menu = Menu::find($id);
+        $validated = $request->validate([
+            'NAMA_PRODUK' => 'sometimes|string|max:50',
+            'HARGA_DASAR' => 'sometimes|integer',
+            'HARGA_JUAL'  => 'sometimes|integer',
+        ]);
+        $menu->update($validated);
         return $menu;
     }
 
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -9,14 +9,25 @@ class Menu extends Model
     protected $table = 'menu';
     protected $primaryKey = 'ID_PRODUK';
     public $incrementing = false;
-    public $timestamps = false;
+    protected $keyType = 'string';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'ID_PRODUK', 
+        'NAMA_PRODUK', 
+        'HARGA_DASAR', 
+        'HARGA_JUAL'
+    ];
 
     public function transaksi()
     {
         return $this->belongsToMany(
             Transaksi::class,
+            'detail_transaksi',
             'ID_PRODUK',
             'ID_TRANSAKSI'
         );
     }
 }
+
+
