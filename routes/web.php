4263384acb60caadc14d568c588c98f714f457ae
Bagaseka27 <ekasaputrabagas62@gmail.com>
2025-12-01 08:12:20 +0700

    revroute1

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,30 @@ Route::get('/dashboard', function () {
 
 // --- 3. MANAJEMEN MENU ---
 Route::get('/menu', function () {
-    return view('pages.menu'); // Pastikan kamu buat file: resources/views/pages/menu.blade.php
+    return view('pages.menu'); 
 })->name('menu');
 
 // --- 4. KASIR / POS (Khusus Barista) ---
 Route::get('/pos', function () {
-    return view('pages.pos'); // Pastikan kamu buat file: resources/views/pages/pos.blade.php
+    return view('pages.pos');
 })->name('pos');
 
 // --- 5. DATA KARYAWAN (Khusus Admin) ---
 Route::get('/employees', function () {
-    return view('pages.employee'); // Pastikan kamu buat file: resources/views/pages/employee.blade.php
+    return view('pages.employee'); 
 })->name('employees');
 
 // --- 6. HISTORY TRANSAKSI ---
 Route::get('/history', function () {
-    return view('pages.history'); // Buat file: resources/views/pages/history.blade.php
+    return view('pages.history'); 
 })->name('history');
 
 // --- 7. STOK / INVENTORY ---
 Route::get('/inventory', function () {
-    return view('pages.inventory'); // Buat file: resources/views/pages/inventory.blade.php
+    return view('pages.inventory');
 })->name('inventory');
 
 // --- 8. LOKASI & CABANG ---
 Route::get('/locations', function () {
-    return view('pages.locations'); // Buat file: resources/views/pages/locations.blade.php
+    return view('pages.locations');
 })->name('locations');
